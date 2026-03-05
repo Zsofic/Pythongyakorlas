06334f41b7f5746944167f1e63ec5e8920d96f83
Zsofic <kasazsofi23@gmail.com>
2026-03-05 17:33:02 +0100

    push gyakorlas

---
+++ Masodik.py
@@ -0,0 +1,28 @@
+def feldolgozas(kerdes):
+    print("Feldolgozas alatt!")
+
+    szamjegyek = dict()
+    for betu in kerdes:
+        #print(betu)
+        if betu in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"):
+            if betu in szamjegyek:
+                szamjegyek[betu] += 1
+            else:
+                szamjegyek[betu] = 1
+    return szamjegyek
+
+    #return kerdes[::-1]
+
+while True:
+    kerdes = input("Kerdes: ")
+
+    if kerdes == "exit":
+        print("Viszlát!")
+        break
+
+    print("Ezt kerdezte: " + kerdes)
+
+    valasz=feldolgozas(kerdes)
+    print("Valasz: " + str(valasz))
+print("Program vege!")
+
