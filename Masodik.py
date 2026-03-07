db235d2b2028dd8130da6449f400a5ee83228083
Zsofic <kasazsofi23@gmail.com>
2026-03-07 15:48:10 +0100

    push

--- Masodik.py
+++ Masodik.py
@@ -3,12 +3,25 @@ def feldolgozas(kerdes):
 
     szamjegyek = dict()
     for betu in kerdes:
-        #print(betu)
+        # print(betu)
         if betu in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"):
             if betu in szamjegyek:
                 szamjegyek[betu] += 1
             else:
                 szamjegyek[betu] = 1
+
+    #1. feladat
+    if kerdes[-1] == "?":
+        print("Ez bizony egy kérdés")
+
+    #3. feladat
+    if len(szamjegyek) == 0:
+        print("Ebben egy számjegy sem volt.")
+
+    #4. feladat
+    pontszamlalo = kerdes.count(".")
+    print("A kérdés", pontszamlalo ,"darab pontot tartalmazott.")
+
     return szamjegyek
 
     #return kerdes[::-1]
@@ -20,9 +33,15 @@ while True:
         print("Viszlát!")
         break
 
+    #2. feladat
+    if kerdes == "quit":
+        print("Bye!")
+        break
+
     print("Ezt kerdezte: " + kerdes)
 
-    valasz=feldolgozas(kerdes)
+    valasz = feldolgozas(kerdes)
     print("Valasz: " + str(valasz))
+
 print("Program vege!")
 
