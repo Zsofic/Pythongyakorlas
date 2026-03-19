e1971162c4b0c3abe92ccf0249d5d4ca2cae1c51
Zsofic <kasazsofi23@gmail.com>
2026-03-19 17:09:46 +0100

    push gyakorlas

--- Harmadik.py
+++ Harmadik.py
@@ -1,3 +1,20 @@
+import csv
 
-if __name__ == "__main__":
-print("MODULNEV:" + __name__)
+with open("teszt.csv", "r", encoding="utf-8", newline="") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
+        print(row[1])
+        print(row[3])
+
+        print("Oszlopok:" str(len(row)))
+
+        for oszlop in row:
+            print("O:" + oszlop)
+
+        with open("teszt.csv", "r", encoding="utf-8", newline="") as f:
+            olvaso = csv.reader(f)
+            for row in reader:
+                print(row)
+
+                print("Cím:")
